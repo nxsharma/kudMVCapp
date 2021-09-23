715fb69523da1bea195e04c845482a01a34af309
Nikhil Sharma <nikhil.kay@gmail.com>
2021-09-23 14:31:33 -0500

    del var

--- KudMVCapp/Models/EmployeeContext.cs
+++ KudMVCapp/Models/EmployeeContext.cs
@@ -9,7 +9,7 @@ namespace KudMVCapp.Models
     //model stuff to get data
     public class EmployeeContext : DbContext
     {
-        public string s;
+        
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
     }
